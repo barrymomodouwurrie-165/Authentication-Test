ea755aaa680e9879ace88249104b020686cffea1
momodou165 <barrymomodouwurry165@gmail.com>
2026-05-26 22:01:36 +0000

    made the login page function and connect it to the backend

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,22 @@ import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
 import ContentsPage from "./pages/ContentsPage";
 import { Routes, Route } from "react-router";
-import "./App.css"
-import "./pages/SignupPage.css"
-import "./pages/LoginPage.css"
-// import { useState } from "react";
+import "./App.css";
+import "./pages/SignupPage.css";
+import "./pages/LoginPage.css";
+import { useState } from "react";
 
 function App() {
-  // const [user, setUser] = useState({})
+  const [user, setUser] = useState();
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/login"
+          element={<LoginPage user={user} setUser={setUser} />}
+        />
         <Route path="/contents" element={<ContentsPage />} />
       </Routes>
     </>
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,24 +1,67 @@
-// import React from 'react'
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router";
+const LoginPage = (user, setUser) => {
+  const [signEmail, setSignEmail] = useState("");
+  const [signPassword, setSignPassword] = useState("");
+  const [alter, setAlter] = useState("Show");
+  const [isShowing, setIsShowing] = useState(false);
+  const Navigate = useNavigate();
 
-const LoginPage = () => {
+  const handlePassword = () => {
+    if (alter === "Show") {
+      setAlter("Hide");
+      setIsShowing(true);
+    }
+    if (alter === "Hide") {
+      setAlter("Show");
+      setIsShowing(false);
+    }
+  };
+
+  const handleLogin = async () => {
+    if (signEmail !== "" && signPassword !== "") {
+      const response = await axios.post(
+        "http://localhost:5000/api/users/login",
+        {
+          email: signEmail,
+          password: signPassword,
+        },
+      );
+      setSignEmail("");
+      setSignPassword("");
+      Navigate("/contents");
+      console.log(response.data);
+      
+    }
+  };
   return (
     <>
       <h1 className="sign-in-header-title">Log In here</h1>
       <div className="sign-in-form-container">
-        <form action="">
-          <p className="sign-in-para-email">Email</p>
-          <input type="email" />
-          <p className="sign-in-para-password">Password</p>
-          <div>
-
-          <input type="text" />
-          <button className="sign-in-hide-button">Hide</button>
-          </div>
-          <button className="sign-in-create-button">Sign In</button>
-        </form>
+        <p className="sign-in-para-email">Email</p>
+        <input
+          type="email"
+          onChange={(e) => setSignEmail(e.target.value)}
+          value={signEmail}
+        />
+        <p className="sign-in-para-password">Password</p>
+        <div>
+          <input
+            type={isShowing ? "text" : "password"}
+            onChange={(e) => setSignPassword(e.target.value)}
+            value={signPassword}
+          />
+          <button className="sign-in-hide-button" onClick={handlePassword}>
+            {alter}
+          </button>
+        </div>
+        <button className="sign-in-create-button" onClick={handleLogin}>
+          Sign In
+        </button>
       </div>
     </>
   );
-}
+};
 
-export default LoginPage
+export default LoginPage;
