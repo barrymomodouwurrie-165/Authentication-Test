762826ecaf69fb699be7780353a42d7e190e10af
momodou165 <barrymomodouwurry165@gmail.com>
2026-05-29 02:20:13 +0000

    made the logout button function by calling the logout route at the backend

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -45,19 +45,31 @@ export const AuthProvider = ({ children }) => {
       }
     }
   };
+  const handleLogout = async () => {
+    try {
+      await axios.post("http://localhost:5000/api/users/logout");
+      setUser({});
+      localStorage.clear();
+      navigate("/");
+    } catch (error) {
+      const message = error;
+      console.log(message);
+    }
+  };
   useEffect(() => {
     const checkRefresh = async () => {
       const result = await axios.post(
         "http://localhost:5000/api/users/refresh_token",
       );
       setUser((prev) => ({ ...prev, accessToken: result.data.accessToken }));
-      console.log(result.data)
+      console.log(result.data);
     };
     checkRefresh();
   }, []);
   return (
     <AuthContext.Provider
       value={{
+        handleLogout,
         user,
         handleLogin,
         signEmail,
--- src/pages/ContentsPage.jsx
+++ src/pages/ContentsPage.jsx
@@ -6,7 +6,7 @@ import "./Navbar.css";
 import "./Comments.css";
 
 const ContentsPage = () => {
-  const { user } = useAuth();
+  const { user, handleLogout } = useAuth();
 
   if (!user?.accessToken) {
     <Navigate to="/login" replace />;
@@ -15,7 +15,7 @@ const ContentsPage = () => {
     <>
       <div className="nav-container">
         <div className="user-info">Hello, {user.user.name}</div>
-        <button className="logout-button">Logout</button>
+        <button className="logout-button" onClick={handleLogout}>Logout</button>
       </div>
       <h3>Kindly tell us about yourself</h3>
       <div>
